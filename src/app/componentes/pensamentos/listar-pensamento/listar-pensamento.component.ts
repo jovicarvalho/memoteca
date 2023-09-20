4c3574235b8a88a5f2b9ae23afd794580f9de549
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-09-20 19:26:27 -0300

    ✨feat: router link in create

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    //{
+     // conteudo:'Passo informações para o Componente Filho',
+      //autoria: 'Componente Pai',
+      //modelo:'modelo3'
+    //},
+    //{
+      //conteudo:'Minha propriedade é decorada com @input',
+      //autoria: 'Componente Filho',
+      //modelo:'modelo2'
+    //},
+    //{
+      //conteudo:'Outro filho',
+      //autoria: 'Sou da família de componentes',
+    //  modelo:'modelo1'
+    //}
+  ];
   constructor() { }
 
   ngOnInit(): void {
