5d4d507154b0ff8bb2465043d17b4209eb794c6a
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-09-20 19:27:04 -0300

    ✨ new component: pensamento

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento ={
+    conteudo:'I love Angular',
+    autoria: 'Nay',
+    modelo:'modelo3'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
