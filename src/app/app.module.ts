4e43df00eaf6398792b73c90c526bdf9dbb2dea6
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-09-22 10:32:29 -0300

    ✨feat: validators in create-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
@@ -29,6 +29,7 @@ import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pens
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule
    
   ],
   providers: [],
--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,30 +10,44 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-    conteudo:'',
-    autoria:'',
-    modelo: ''
-  }
+  
+  formulario!: FormGroup;
 
   constructor(
     private service:PensamentoService,
-    private router: Router
+    private router: Router,
+    private formbuilder: FormBuilder
     ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formbuilder.group({
+      conteudo:['',Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])], 
+      autoria:['',Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+        ])],
+      modelo: ['modelo1',[Validators.required]]
+    })
   }
 
   criarPensamento(){
-    this.service.criar(this.pensamento).subscribe(()=>{
-      alert("Pensamento adicionado com sucesso!")
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(()=>{
+        alert("Pensamento adicionado com sucesso!")
+        this.router.navigate(['/listarPensamento'])}
+      )
+    }
   }
   
   cancelar(){
     alert("Pensamento Cancelado")
     this.router.navigate(['/listarPensamento'])
   }
+
 
 }
